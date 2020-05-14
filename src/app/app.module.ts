a29d8aedbff5e373bb5c496c5cd2a200c6fad9ef
Zack <aminuzack7@gmail.com>
2020-05-14 12:53:42 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { ProductsComponent } from './components/products/products.component';
+import { TeamComponent } from './components/team/team.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { ProgramsComponent } from './components/programs/programs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactUsComponent,
+    ProductsComponent,
+    TeamComponent,
+    TestimonialsComponent,
+    ProgramsComponent
   ],
   imports: [
     BrowserModule,
