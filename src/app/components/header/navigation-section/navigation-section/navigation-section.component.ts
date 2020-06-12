5ddb9498c95871ba468ee7bece9f5d6a82185dfc
Zack <aminuzack7@gmail.com>
2020-06-12 01:55:11 +0100

    committing latest changes to fixes

--- src/app/components/header/navigation-section/navigation-section/navigation-section.component.ts
+++ src/app/components/header/navigation-section/navigation-section/navigation-section.component.ts
@@ -11,27 +11,23 @@ export class NavigationSectionComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    // jQuery(".sp-megamenu-wrapper li a").click(function (event) {
-    //     event.preventDefault();
-    //     var $self = jQuery(this);
-    //     var $this = jQuery(this).parent();
+    jQuery(".sp-megamenu-wrapper li a").click(function (event) {
+        event.preventDefault();
+        var $self = jQuery(this);
+        var $this = jQuery(this).parent();
 
-    //     if($this.hasClass('active')) {
-    //       return;
-    //     }
+        if($this.hasClass('active')) {
+          return;
+        }
 
-    //     $self.closest('ul').children().removeClass('active');
-    //     $self.parent().addClass('active');      
-    // })
+        $self.closest('ul').children().removeClass('active');
+        $self.parent().addClass('active');      
+    })
 
-    // jQuery(".sp-dropdown-inner li a").click(function (event) {
-    //   event.preventDefault();
-    //   if(jQuery(".sp-megamenu-wrapper li a").hasClass("active")){
-    //     jQuery(".sp-megamenu-wrapper li a").removeClass('active');
-    //     var parent = jQuery(".sp-dropdown-inner li a");
-    //     parent.addClass('active');
-    //   };      
-    // })
+    jQuery(".sp-dropdown-inner li a").click(function (event) {
+      event.preventDefault();
+      jQuery('.sp-menu-item').removeClass('active');   
+    })
   }
 
 }
