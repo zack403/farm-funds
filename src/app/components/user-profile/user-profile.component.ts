6a79a99ae1722a5a897ae80b53169bf1c4c2a540
Zack <aminuzack7@gmail.com>
2020-09-02 17:07:09 +0100

    updated api url to use production

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -118,7 +118,7 @@ export class UserProfileComponent implements OnInit {
      const {email, firstName, lastName} = this.authSvc.getCurrentUserData();
        try {
            var handler = PaystackPop.setup({
-             key: 'pk_test_801d715bb68f121b21aac949a0f65b3a93dfb3d0',
+             key: 'pk_test_e7861b0c2ca5ed383dc4d2934270218b5a88e292',
              email: email,
              amount: amt*100,
              currency: "NGN",
