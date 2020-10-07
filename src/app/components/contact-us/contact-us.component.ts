3609ed79c53b98901f08519c36ad97887178ac8d
Zack <aminuzack7@gmail.com>
2020-10-07 17:56:34 +0100

    implemented contact us feature

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToasterService } from 'src/app/services/toaster.service';
+import { UtilityService } from 'src/app/services/utility.service';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
-
-  constructor() { }
+  contactUsForm: FormGroup;
+  isBusy: boolean = false;
+  constructor(private formBuilder: FormBuilder, private utilSvc: UtilityService, private toastrSvc: ToasterService) { }
 
   ngOnInit() {
+    this.contactUsForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      subject: [''],
+      message: ['', [Validators.required]]
+    });
+  }
+
+  sendUsAMessage() {
+    this.isBusy = true;
+    this.utilSvc.contactUs(this.contactUsForm.value).subscribe((res: any) => {
+      this.isBusy = false;
+      this.toastrSvc.Success(res.message);
+      this.contactUsForm.reset();
+    }, (error) => {
+        this.isBusy = false;
+    })
   }
 
 }
