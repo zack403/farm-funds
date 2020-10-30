4cc8b00dd91520719b0ad6d8695a504af6920fcd
Zack <aminuzack7@gmail.com>
2020-10-30 05:49:43 +0100

    admin dashboard fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
 
    baseUrl: 'http://localhost:3000/api/v1/',
   imagePath: "http://localhost:3000",
-  paystack_key: 'pk_test_801d715bb68f121b21aac949a0f65b3a93dfb3d0'
-  // paystack_key: 'pk_test_e7861b0c2ca5ed383dc4d2934270218b5a88e292'
+  //paystack_key: 'pk_test_801d715bb68f121b21aac949a0f65b3a93dfb3d0'
+   paystack_key: 'pk_test_e7861b0c2ca5ed383dc4d2934270218b5a88e292'
 
 };
