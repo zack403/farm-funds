81e111ebc5ecfa8044627655da73ac837960476c
Zack <aminuzack7@gmail.com>
2021-01-03 19:23:27 +0100

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -101,8 +101,8 @@ export function tokenGetter() {
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
-        //allowedDomains: ['farmfunds.herokuapp.com'],
-        allowedDomains: ['localhost:3000'],
+        allowedDomains: ['farmfunds.herokuapp.com'],
+        //allowedDomains: ['localhost:3000'],
         disallowedRoutes: [],
       },
     }),
